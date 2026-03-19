4a62ffe93fad5a7fa7225a7f18588e2b408a58ea
Davi Oliveira Januário <davi.ojanu@gmail.com>
2026-03-19 20:15:31 -0300

    Correct square calculation for odd and even numbers
    Fix the logic to handle even and odd cases correctly.

--- lista/15.go
+++ lista/15.go
@@ -9,11 +9,15 @@ func main() {
 
 	if n%2 > 0 {
 		for n-1 > 0 {
+			q = (n - 1) * (n - 1)
+			fmt.Printf("%d^2 = %d\n", (n - 1), q)
+			n -= 2
+		}
+	} else {
+		for n > 0 {
 			q = n * n
 			fmt.Printf("%d^2 = %d\n", n, q)
 			n -= 2
 		}
-	} else {
-		fmt.Printf("%d^2 = %d", n, q)
 	}
 }
