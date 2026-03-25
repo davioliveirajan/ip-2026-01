36b8540414c2f97352f9d58068996c59e38748b7
Davi Oliveira Januário <davi.ojanu@gmail.com>
2026-03-25 20:53:40 -0300

    Add program to check if a number is odd or even

---
+++ lista1/01.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Printf("Digite um número: ")
+	fmt.Scan(&n)
+
+	if n%2 == 1 {
+		fmt.Printf("Ímpar\n")
+	} else {
+		fmt.Printf("Par\n")
+	}
+
+}
